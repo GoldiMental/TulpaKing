96abf4580ba54e32c8a95ad07af95f4b04c1c0ff
Jaymental <djohnnyhoffmann@gmail.com>
2025-09-17 13:24:54 +0200

    Update from Jaymental Version: 0.350

--- Skript/Start.js
+++ Skript/Start.js
@@ -82,7 +82,7 @@ async function InitializeGameWorld() {
     console.log("Führe InitializeGameWorld() aus...");
     const movementGame = document.getElementById("movement_game");
     movementGame.classList.toggle("hidethis", true);
-    if (Player.tulpaGegeben) { Player.questLine = { tulpaGegeben: true, } }else{Player.questLine = { tulpaGegeben: false, }};
+    if (Player.tulpaGegeben) { Player.questLine = { tulpaGegeben: Player.tulpaGegeben, } }
     setcolor();
     if (Player.MapX != 0 && Player.MapY != 0 && Player.MapX != undefined && Player.MapY != undefined) {
         console.log("Karte:", Player.actualMap);
